6c173f1bc796fc7eecf72d12723087b398848e06
Tristan Jung <3stan.jung@gmail.com>
2017-11-04 21:39:04 -0700

    fetching items from the right place

--- script/index.js
+++ script/index.js
@@ -2,12 +2,12 @@ var buildings = {}
 var items = {}
 
 $(document).ready(function() {
-    $.getJSON('/resources/buildings.json', dataType="json", function(data) {
+    $.getJSON('https://raw.githubusercontent.com/3stan/roihelper/master/resources/buildings.json', dataType="json", function(data) {
       $(data).each(function() {
         buildings[this.name] = this.items
       })  
     });
-    $.getJSON('/resources/items.json', dataType="json", function(data) {
+    $.getJSON('https://raw.githubusercontent.com/3stan/roihelper/master/resources/items.json', dataType="json", function(data) {
       $(data).each(function() {
         items[this.name] = this.req
       })  
