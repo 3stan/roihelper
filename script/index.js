1fd9731f5b8243651d1ed0c5abd6513d3c377eae
Tristan Jung <3stan.jung@gmail.com>
2017-11-04 22:35:11 -0700

    adding modals for loading

--- script/index.js
+++ script/index.js
@@ -2,25 +2,40 @@ class BuildingHelper {
   constructor(self) {
     this.buildings = {}
     this.items = {}
+
+    this.selected = []
   }
 
   setup(helper) {
-    $.getJSON('https://raw.githubusercontent.com/3stan/roihelper/master/resources/buildings.json', function(data) {
-      $(data).each(function(name, value) {
-        helper.buildings[value.name] = value.items
-      })  
-    })
-    $.getJSON('https://raw.githubusercontent.com/3stan/roihelper/master/resources/items.json', function(data) {
-      $(data).each(function(name, value) {
-        helper.items[value.name] = value.req
-      })  
-      $("#item-field").typeahead({ source:Object.keys(helper.items) })
-    })
+    return new Promise((resolve, reject) => {
+      $.getJSON('https://raw.githubusercontent.com/3stan/roihelper/master/resources/buildings.json', function(data) {
+        $(data).each(function(name, value) {
+          helper.buildings[value.name] = value.items
+        })  
+      })
+      $.getJSON('https://raw.githubusercontent.com/3stan/roihelper/master/resources/items.json', function(data) {
+        $(data).each(function(name, value) {
+          helper.items[value.name] = value.req
+        })  
+        $("#item-field").typeahead({ source:Object.keys(helper.items) })
+      })
 
+      if (true) {
+        resolve("setup complete")
+      } else {
+        reject("something went wrong...")
+      }
+    })
   }  
 }
 
 $(document).ready(function() {
   var helper = new BuildingHelper()
-  helper.setup(helper)
+  helper.setup(helper).then(result => {
+    console.log(result)
+    $("#title").text("Loaded!")
+    setTimeout(function () {$("#title").remove()}, 1000)
+  }, error => {
+    console.log(error)
+  })
 });
